31d48912f22ee5025fcba78f63a369f52c131671
Jan Jongboom <janjongboom@gmail.com>
2016-10-10 13:22:48 +0200

    Fix example 5

--- 5_adding_sensors/main.h
+++ 5_adding_sensors/main.h
@@ -1,33 +1,42 @@
-#include "mbed_events.h"
-#include "mbed.h"       // this tells us to load mbed related functions
+#include <mbed-events/events.h>
+#include <mbed.h>       // this tells us to load mbed related functions
 #include "SimpleBLE.h"  // the bluetooth library
 
 static EventQueue eventQueue(10 * 32);   // use an event queue to dispatch events
 
 SimpleBLE ble("YOUR_NAME_HERE", 100);    // change this to your *unique* name
 
+DigitalOut led1(D4);         // we create a variable 'led1', use it as an out port
+
 DigitalOut alivenessLed(LED1);
 
-SimpleChar<uint32_t> mySensorState = ble.readOnly_u32(0x6710, 0x6711);
-
-void read_sensor() {
-    // YOUR CODE HERE
-    // mySensorState = /* read value from your sensor */
+void updateLedOverBle(bool newValue) {  // a new value was written to the LED characteristic
+    led1 = newValue;
 }
+
+// we create the LED state, which you can read & write, plus a callback method
+SimpleChar<bool> ledState = ble.readWrite_bool(0x6810, 0x6811, &updateLedOverBle);
+
+// This is the sensor state, which we can only read
+SimpleChar<uint32_t> mySensorState = ble.readOnly_u32(0x6920, 0x6921);
 
 void alive() {
     alivenessLed = !alivenessLed;
 }
 
+// YOUR CODE HERE
+// void readSensorState() {
+//     mySensorState = mySensorState + 1;
+// }
+
 // this code runs when the microcontroller starts up
 int main() {
-    Ticker t;
-    t.attach(&alive, 0.5f);
+    // Use the event queue to process events, rather than Tickers
+    eventQueue.post_every(500, alive);
+
+    eventQueue.post_every(2000, readSensorState);
 
     ble.start(&eventQueue);
-
-    Ticker t2;
-    t2.attach(&read_sensor, 2.0f);
 
     while (1) {
         eventQueue.dispatch();
